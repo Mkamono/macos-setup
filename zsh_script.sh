aacbcc99bf8a2afa06ff5de9aa7d8205de21218d
Mkamono <chengyaye31+gmail.com@gmail.com>
2025-01-13 21:53:12 +0900

    update

--- zsh_script.sh
+++ zsh_script.sh
@@ -1,3 +1,3 @@
 export MY_CONFIG_DIR=$HOME/macos-setup
 
-cd $MY_CONFIG_DIR && git --no-pager diff && git --no-pager status -s && cd -
+cd $MY_CONFIG_DIR && git --no-pager diff && git --no-pager status -s && cd - > /dev/null
