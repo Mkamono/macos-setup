8fc2e3f1bce5068d08093a554d82e33b94a149e4
Mkamono <chengyaye31+github@gmail.com>
2025-01-14 19:36:16 +0900

    update

--- zsh_script.sh
+++ zsh_script.sh
@@ -3,7 +3,7 @@ export MY_CONFIG_DIR=$HOME/macos-setup
 export XDG_CONFIG_HOME=$HOME/.config # ghostty config path
 
 cd $MY_CONFIG_DIR && \
-    git pull > /dev/null & \
+    git pull && \
     git --no-pager diff && \
     git --no-pager status -s && \
     cd - > /dev/null
