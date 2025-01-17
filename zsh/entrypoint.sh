43d2b32b016874b0a1febb26ebddfe2891825544
Mkamono <chengyaye31+github@gmail.com>
2025-01-17 10:50:10 +0900

    update

--- zsh/entrypoint.sh
+++ zsh/entrypoint.sh
@@ -2,3 +2,6 @@ REPO_DIR=$HOME/macos-setup
 
 source $REPO_DIR/zsh/alias.sh
 source $REPO_DIR/zsh/script.sh
+
+source /opt/homebrew/share/zsh-autocomplete/zsh-autocomplete.plugin.zsh
+source /opt/homebrew/share/google-cloud-sdk/completion.zsh.inc
